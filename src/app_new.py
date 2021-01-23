75dbeab8bb2ceace80b4aebbc51b0ccff4eb1cbd
Chen Zhao <chenzhaouc@gmail.com>
2021-01-22 23:21:35 -0700

    Add scripts to create new version of dashboard

--- src/app_new.py
+++ src/app_new.py
@@ -1,3 +1,4 @@
+# Import packages
 import pandas as pd
 import numpy as np
 
@@ -9,151 +10,88 @@ from altair_saver import save
 alt.renderers.enable("mimetype")
 # Handle large data sets without embedding them in the notebook
 alt.data_transformers.enable("data_server")
-
 # Dashboard packages
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
-# Read in global data, once processed data is ready -> change URL path to load in processed data
-hotels = pd.read_csv(
-    "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-11/hotels.csv"
-)
-
-hotels["adr_ac"] = hotels["adr"] / (hotels["adults"] + hotels["children"])
-hotel_trim = hotels[
-    [
-        "hotel",
-        "arrival_date_month",
-        "adr_ac",
-        "country",
-        "stays_in_weekend_nights",
-        "stays_in_week_nights",
-    ]
-].copy()
-hotel_trim = hotel_trim.replace([np.inf, -np.inf], np.nan).dropna()
-
-# Dataframe for main plot:
-guests_monthly = hotel_trim.groupby("arrival_date_month")["hotel"].count()
-prices_monthly = hotel_trim.groupby("arrival_date_month")["adr_ac"].mean()
-data_monthly = pd.merge(guests_monthly, prices_monthly, on="arrival_date_month")
-data_monthly = data_monthly.reset_index()
-# order by month:
-months_ordered = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-]
-
-data_monthly["arrival_date_month"] = pd.Categorical(
-    data_monthly["arrival_date_month"], categories=months_ordered, ordered=True
-)
-data_monthly = data_monthly.sort_values("arrival_date_month")
-
-# Dataset contains July and August date from 3 years, the other month from 2 years. Normalize data:
-data_monthly.loc[
-    (data_monthly["arrival_date_month"] == "July")
-    | (data_monthly["arrival_date_month"] == "August"),
-    "hotel",
-] /= 3
-data_monthly.loc[
-    ~(
-        (data_monthly["arrival_date_month"] == "July")
-        | (data_monthly["arrival_date_month"] == "August")
-    ),
-    "hotel",
-] /= 2
-
-# Dataframe for countries plot:
-top_20_countries = (
-    hotel_trim.groupby("country")
-    .size()
-    .reset_index(name="counts")
-    .sort_values(by="counts", ascending=False)[:20]
-)
-
-# Dataframe for Stay Length plot:
-hotel_trim["total_nights"] = (
-    hotel_trim["stays_in_weekend_nights"] + hotel_trim["stays_in_week_nights"]
-)
-num_nights = list(hotel_trim["total_nights"].value_counts().index)
-num_bookings = list(hotel_trim["total_nights"].value_counts())
-rel_bookings = (
-    hotel_trim["total_nights"].value_counts() / sum(num_bookings) * 100
-)  # convert to percent
-stay_nights = pd.DataFrame(
-    {
-        "hotel": "Both Hotels",
-        "num_nights": num_nights,
-        "rel_num_bookings": num_bookings,
-    }
-)
-
+# Import functions from data_wrangling script
+from data_wrangling import main_plot, left_plot, right_plot
 
 # Assign column names to a list for dropdown widget
-columns = ["hotel", "adr_ac"]
-
-
-def histogram_1():
-    top_countries = (
-        alt.Chart(top_20_countries, title="Top 20 Home Countries of Guests")
-        .mark_bar()
-        .encode(
-            alt.X("country", sort="-y", title="Countries"),
-            alt.Y("counts", title="Guests numbers"),
-            alt.Tooltip("country"),
-        )
-    )
-    return top_countries.to_html()
-
-
-def histogram_2():
-    stay = (
-        alt.Chart(stay_nights)
-        .mark_bar()
-        .encode(
-            alt.X("num_nights", title="Number of nights"),
-            alt.Y("rel_num_bookings", title="Percent of guests"),
-        )
-    )
-    return stay.to_html()
-
+columns = ["Guest Numbers", "Average daily rate per person", "Required parking spaces"]
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-## Setup app layout and front-end
-app.layout = dbc.Container(
+# Setup app layout and front-end
+jumbotron = dbc.Jumbotron(
     [
-        html.H1("Super-Hotels-Happy-Manager-Info"),
-        html.P("by Sakshi Jain, Trevor Kinsey, Cameron Harris, Chen Zhao"),
+        dbc.Container(
+            [
+                html.H1("Super Hotel Management", className="display-3"),
+                html.P(
+                    "This is an interactive dashboard based on the data comes from the Hotel Booking Demand dataset",
+                    className="lead",
+                ),
+            ],
+        )
+    ],
+    fluid=True,
+)
+
+info_area = dbc.Container(
+    [
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        html.H4("Select variables"),
+                        html.H4("Select Hotel Types"),
+                        html.Br(),
+                        dcc.RadioItems(
+                            id="hotel-type-selection",
+                            options=[
+                                {"label": "All Hotels", "value": "All"},
+                                {"label": "Resort Hotel", "value": "Resort"},
+                                {"label": "City Hotel", "value": "City"},
+                            ],
+                            value="All",
+                            labelStyle={"display": "block"},
+                        ),
+                        html.Br(),
+                        html.Br(),
+                        html.H4("Select Weeks"),
+                        html.Br(),
+                        dcc.RangeSlider(
+                            id="week-selection",
+                            min=1,
+                            max=53,
+                            value=[1, 53],
+                            marks={
+                                1: "1",
+                                14: "14",
+                                27: "27",
+                                40: "40",
+                                53: "53",
+                            },
+                        ),
+                        html.Br(),
+                        html.Br(),
+                        html.H4("Select Features for Main Plot"),
+                        html.Br(),
                         dcc.Dropdown(
                             id="y-axis-dropdown",
                             options=[
                                 {"label": label, "value": label} for label in columns
                             ],
-                            value=columns[1],
+                            value=columns[0],
                             multi=False,
                             searchable=False,
+                            clearable=False,
                         ),
                     ],
                     md=3,
-                    style={"border": "1px solid #d3d3d3", "border-radius": "10px"},
                 ),
                 dbc.Col(
                     [
@@ -173,7 +111,6 @@ app.layout = dbc.Container(
                             [
                                 html.Iframe(
                                     id="hist1",
-                                    srcDoc=histogram_1(),
                                     style={
                                         "border-width": "0",
                                         "width": "50%",
@@ -182,7 +119,6 @@ app.layout = dbc.Container(
                                 ),
                                 html.Iframe(
                                     id="hist2",
-                                    srcDoc=histogram_2(),
                                     style={
                                         "border-width": "0",
                                         "width": "50%",
@@ -198,22 +134,82 @@ app.layout = dbc.Container(
     ]
 )
 
-## Callbacks and back-end
-@app.callback(Output("lines", "srcDoc"), Input("y-axis-dropdown", "value"))
-# Function to plot cross-plot using dropdown x and y variables
-def plot_altair(y_col):
+app.layout = html.Div([jumbotron, html.Br(), info_area])
+
+# Callbacks and back-end
+@app.callback(
+    Output("lines", "srcDoc"),
+    Input("hotel-type-selection", "value"),
+    Input("week-selection", "value"),
+    Input("y-axis-dropdown", "value"),
+)
+# Function to plot the main plot using selected hotel type and y variables
+def plot_altair(hotel_type, x_col, y_col):
+    df = main_plot(hotel_type, x_col)
+    title_text = y_col + " for each week in the year"
     chart = (
-        alt.Chart(data_monthly)
-        .mark_line()
+        alt.Chart(df, title=title_text)
+        .mark_line(color="orange")
         .encode(
-            alt.X("arrival_date_month", title="Month", sort=months_ordered),
-            y=y_col,
-            tooltip=y_col,
+            alt.X("Arrival week", title="Week numbers", axis=alt.Axis(grid=False)),
+            alt.Y(y_col, title=y_col, scale=alt.Scale(zero=False)),
         )
-        .properties(width=700, height=350)
-        .interactive()
+        .properties(width=700, height=370)
+        .configure_axis(labelFontSize=13, titleFontSize=17)
+        .configure_title(fontSize=23)
     )
     return chart.to_html()
+
+
+@app.callback(
+    Output("hist1", "srcDoc"),
+    Input("hotel-type-selection", "value"),
+    Input("week-selection", "value"),
+)
+# Function to plot the bottom left plot using selected hotel type
+def histogram_1(hotel_type, weeks):
+    df = left_plot(hotel_type, weeks)
+    top_countries = (
+        alt.Chart(df, title="Top 20 Home Countries of Guests")
+        .mark_bar(color="orange")
+        .encode(
+            alt.X("Country of origin", sort="-y", title="Countries"),
+            alt.Y("counts", title="Guests numbers"),
+            alt.Tooltip("Country of origin"),
+        )
+        .properties(width=300, height=200)
+        .configure_axis(labelFontSize=10, titleFontSize=15)
+        .configure_title(fontSize=19)
+    )
+    return top_countries.to_html()
+
+
+@app.callback(
+    Output("hist2", "srcDoc"),
+    Input("hotel-type-selection", "value"),
+    Input("week-selection", "value"),
+)
+# Function to plot the bottom right plot using selected hotel type
+def histogram_2(hotel_type, weeks):
+    df = right_plot(hotel_type, weeks)
+    stay = (
+        alt.Chart(df, title="Length of Guests Stay")
+        .mark_bar(clip=True, color="orange")
+        .encode(
+            alt.X(
+                "Number of Nights of Stay",
+                title="Number of nights of stay",
+                scale=alt.Scale(domain=(0, 15)),
+                bin=alt.Bin(maxbins=75),
+            ),
+            alt.Y("Percent of Guests", title="Percent of guests"),
+            alt.Tooltip("Percent of Guests"),
+        )
+        .properties(width=300, height=200)
+        .configure_axis(labelFontSize=10, titleFontSize=15)
+        .configure_title(fontSize=19)
+    )
+    return stay.to_html()
 
 
 if __name__ == "__main__":
